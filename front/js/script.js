c8d539efb59fefdd08ac097222dff14119b139de
vinc5549 <vincente555@hotmail.fr>
2022-06-17 13:48:19 +0200

    utilisation createElement

--- front/js/script.js
+++ front/js/script.js
@@ -1,62 +1,58 @@
 // recovery of Api content and insertion in DOM
 async function getProducts() {
      await fetch("http://localhost:3000/api/products")
-     .then(res => res.json())
-     .then(produit => {
-         for (const item of produit) {
-             document.querySelector("section").innerHTML +=
-                 `<a href="./product.html?id=${item._id}">
-                     <article>
-                         <img src="${item.imageUrl}" alt="${item.altTxt}">
-                         <h3 class="productName">
-                             ${item.name}
-                         </h3>
-                         <p class="productDescription">
-                         ${item.description}
-                         </p>
-                     </article>
-                 </a>`
-         }
-     })
-     .catch(function(err) {
-         console.log("erreur")
-     })
- }
+     .then(function(res) {
+        if (res.ok) {
+            return res.json();
+    }
+    })
+    .then(function(value) {
+        let kanapList = value;
+        for (let kanap of kanapList) {
+            kanapCard(kanap);
+        }
+    });
+    function kanapCard(kanap){
+        let element = document.querySelector(".items");
+        let newLink = document.createElement("a");
+        let newArticle = document.createElement("article");
+        let newImage = document.createElement("img");
+        let newTitle = document.createElement("h3");
+        let newText = document.createElement("p");
+    
+        newTitle.setAttribute("class", "productName");
+        newText.setAttribute("class", "productDescription");
+        newLink.href ="./product.html?id=" + kanap._id;
+        newImage.src = kanap.imageUrl;
+        newImage.alt = kanap.altTxt;
+        newTitle.textContent = kanap.name;
+        newText.textContent = kanap.description;
+    
+        element.appendChild(newLink);
+        newLink.appendChild(newArticle);
+        newArticle.appendChild(newImage);
+        newArticle.appendChild(newTitle);
+        newArticle.appendChild(newText);
+    }
+     }
  //execution of the function
  getProducts()
 
 
- 
-/*
-let meubleData = [];
-
-const fetchMeuble = async () => {
-await fetch("http://localhost:3000/api/products")
-.then((res) => res.json())
-.then((promise) => {
-     meubleData = promise
-     console.log(meubleData);
-});
-};
-
-const meubleDisplay = async () => {
-     await fetchMeuble();
-
-     document.getElementById("items").innerHTML = `<div><img class="banniere"
-      src="${meubleData[3].imageUrl}" alt="image items du site" "/></div>`
-      
-};
-
-meubleDisplay();
-*/
-
-
-
-/*const img = document.getElementById('items');
-
-fetch('http://localhost:3000/api/products')
-     .then(res => res.json())
-     .then(data => img.src = data[0].url)
- 
+ /*
+ for (const item of produit) {
+    document.querySelector("section").innerHTML +=
+        `<a href="./product.html?id=${item._id}">
+            <article>
+                <img src="${item.imageUrl}" alt="${item.altTxt}">
+                <h3 class="productName">
+                    ${item.name}
+                </h3>
+                <p class="productDescription">
+                ${item.description}
+                </p>
+            </article>
+        </a>`
+}
 */
 
